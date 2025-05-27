29250208fe36042285e0ca0015d8f3ed5bd1b634
ahmedamradly <139693442+ahmedamradly@users.noreply.github.com>
2025-05-27 03:38:25 +0300

    Add files via upload

---
+++ interactivity.js
@@ -0,0 +1,8 @@
+
+  function changeText() {
+    let msg = document.getElementById("message");
+    msg.innerHTML = "You're amazing!";
+    msg.style.color = "blue";
+    msg.style.fontSize = "28px";
+    document.body.style.backgroundColor = "#fceabb";
+  }
